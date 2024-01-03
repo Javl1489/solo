1877a86dceee8be9f1e3cbf8447257f246d42c3d
javl1489 <154706475+Javl1489@users.noreply.github.com>
2024-01-03 20:42:00 +0000

    basededatos

---
+++ Basedatos1.py
@@ -0,0 +1,25 @@
+encabezado = print("hola vienbenido")
+
+paso1_str = input("para continuar")
+
+paso1_str = int(paso1_str)
+
+if paso1_str == 1489:
+    print("continuemos")
+
+if paso1_str == 1489:
+    print("Johnny vasco")
+
+if paso1_str == 1478:
+    print("continuemos")
+
+if paso1_str == 1478:
+    print("estela")
+
+
+
+
+else:
+    print("no coninua")    
+
+  
