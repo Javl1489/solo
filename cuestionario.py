95fc6a202f458e08278155eb679a3d1e4556a413
javl1489 <154706475+Javl1489@users.noreply.github.com>
2024-01-03 16:19:20 +0000

    cuestionario

---
+++ cuestionario.py
@@ -0,0 +1,13 @@
+nombre = input("cual es tu nombre: ")
+
+edad_str = input("cual es tu edad: ")
+
+cedula = input("cual es tu cedula: ")
+
+edad_str =int(edad_str)
+
+if edad_str >= 18:
+    print("puedes ingresar")
+
+else:
+    print("no puedes ingresar")
